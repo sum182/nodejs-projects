4db2353d56ca884b69be8bc6fed19a2986044833
sum182 <sum182@gmail.com>
2022-03-07 15:48:05 -0300

    add novo projeto

---
+++ javascript-definitions/index.js
@@ -0,0 +1,18 @@
+
+const libsmStrings = require('./../../../lib/sum182/smStrings');
+
+
+function main(){
+    try {
+        console.clear;
+        libsmStrings.logConsoleHighlighted('App javascript-definitions em execução');
+        console.log('');
+        
+    } catch (error) {
+        console.log('Erro ao relizar processamento!');
+    }
+
+}
+
+//Executa método principal
+main();
