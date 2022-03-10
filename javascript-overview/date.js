33bf60dcb5c529de4e61dee7352d1e4bbfb3749b
sum182 <sum182@gmail.com>
2022-03-10 15:38:36 -0300

    new files

---
+++ javascript-overview/date.js
@@ -0,0 +1,20 @@
+const libsmStrings = require('./../../../lib/sum182/smStrings');
+
+function testDates (){
+    console.log('');
+    libsmStrings.logConsoleHighlighted('Testando Dates');   
+    console.log("");
+
+    
+
+
+   
+
+
+    
+
+}
+
+module.exports = {
+    testDates
+}
